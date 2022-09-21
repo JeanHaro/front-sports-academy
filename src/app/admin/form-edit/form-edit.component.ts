d3f5b80815df9796e5a8a1e386708f4259fb653f
Jean Haro <JeanCHL98@hotmail.com>
2022-09-21 18:41:05 -0500

    Creando los componentes y realizando la estructura del trabajo

---
+++ src/app/admin/form-edit/form-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-edit',
+  templateUrl: './form-edit.component.html',
+  styleUrls: ['./form-edit.component.scss']
+})
+export class FormEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FormComponent } from './pages/form/form.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavComponent } from './admin/nav/nav.component';
+import { ScheduleFormComponent } from './admin/schedule-form/schedule-form.component';
+import { StudentsListComponent } from './admin/students-list/students-list.component';
+import { StudentEditComponent } from './admin/student-edit/student-edit.component';
+import { StudentFormComponent } from './admin/student-form/student-form.component';
+import { ScheduleListComponent } from './admin/schedule-list/schedule-list.component';
+import { ScheduleEditComponent } from './admin/schedule-edit/schedule-edit.component';
+import { FormListComponent } from './admin/form-list/form-list.component';
+import { FormEditComponent } from './admin/form-edit/form-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent,
+    HomeComponent,
+    ContactComponent,
+    FormComponent,
+    AboutUsComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ScheduleFormComponent,
+    StudentsListComponent,
+    StudentEditComponent,
+    StudentFormComponent,
+    ScheduleListComponent,
+    ScheduleEditComponent,
+    FormListComponent,
+    FormEditComponent,
   ],
   imports: [
     BrowserModule,
