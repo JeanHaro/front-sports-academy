c7ce942ab9a38d991f45fa94ab3fddc15f432f98
Jean Haro <JeanCHL98@hotmail.com>
2022-10-27 17:45:16 -0500

    Validaciones del formulario

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -10,7 +10,9 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  // Validaciones del formulario
+  public formSubmitted = false;
+
+  // TODO: Validaciones del formulario
   public registerForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     password: ['', Validators.required]
@@ -21,7 +23,24 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // TODO: Crear admin
   crearAdmin() {
+    this.formSubmitted = true;
     console.log(this.registerForm.value);
+
+    // Verificar que el formulario es correcto al crear
+    if (this.registerForm.valid) {
+      console.log('Posteando formulario');
+    } else {
+      console.log('Formulario no es correcto...');
+    }
+  }
+
+  // TODO: Si el campo no es valido
+  campoNoValido (campo: string): boolean {
+    // Si se envió y no es valido
+    if (this.registerForm.get(campo)?.invalid && this.formSubmitted) return true;
+    
+    return false;
   }
 }
