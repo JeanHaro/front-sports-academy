7bacc7bddab44891c6b2e6d3cf1cec0da63310b4
Jean Haro <JeanCHL98@hotmail.com>
2022-10-27 21:53:44 -0500

    Guardando token en el localstorage

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 
+// Rxjs
+import { tap } from 'rxjs/operators'
+
 // Interfaces
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form.interface';
@@ -21,11 +24,19 @@ export class AdminService {
 
   // TODO: Crear Admin
   crearAdmin (formData: RegisterForm) {
-    return this.http.post(`${base_url}/admin`, formData);
+    return this.http.post(`${base_url}/admin`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token)
+      })
+    )
   }
 
   // TODO: Iniciar sesión
   login (formData: LoginForm) {
-    return this.http.post(`${base_url}/auth`, formData);
+    return this.http.post(`${base_url}/auth`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.token)
+      })
+    )
   }
 }
