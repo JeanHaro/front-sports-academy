ffc584c9bfc2ca1d26d743411a22722a962a38a0
Jean Haro <JeanCHL98@hotmail.com>
2022-11-06 22:58:15 -0500

    Añadiendo validaciones propias al form

--- src/app/pages/form/components/form.component.ts
+++ src/app/pages/form/components/form.component.ts
@@ -4,8 +4,11 @@ import { Router } from '@angular/router';
 // SweetAlert2
 import Swal from 'sweetalert2';
 
+// Date-fns
+import { format } from 'date-fns';
+
 // Formulario
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, AbstractControlOptions, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 // Interfaces
 import { ScheduleForm } from 'src/app/interfaces/schedules-form.interface';
@@ -47,6 +50,14 @@ export class FormComponent implements OnInit {
     this.obtenerHorarios();
   }
 
+  // TODO: Validaciones propias
+  formOptions: AbstractControlOptions = {
+    validators: [
+      this.validarDNI('dni'), 
+      this.validarCelular('celular')
+    ]
+  }
+
   // TODO: Estructura y validación del formulario
   private buildForm() {
     this.enrollmentForm = this.fb.group({
@@ -60,7 +71,7 @@ export class FormComponent implements OnInit {
       hora: ['', Validators.required],
       codigo: ['', Validators.required],
       horario: ['', Validators.required],
-    })
+    }, this.formOptions);
   }
 
   // TODO: Obtener horarios
@@ -86,16 +97,33 @@ export class FormComponent implements OnInit {
   obtenerHora() {
     let { edad, turno } = this.enrollmentForm.value;
     
+    // Fecha hoy
+    let year = new Date().getFullYear();
+    let month = new Date().getMonth();
+    let day = new Date().getDate();
+    // date-fns
+    let today = format(new Date(year, month, day), 'yyyy-MM-dd');
+
     // Obtener datos del horario mediante la edad
     const hora_edad = this.horarios.filter(
       horario => {
-        if (edad >= horario.edad_min && edad <= horario.edad_max) {
-          return true;
-        } else {
-          this.valores.valueHour = '';
+        // Fecha inicial del horario
+        let yearS = new Date(horario.fecha_inicial).getUTCFullYear();
+        let monthS = new Date(horario.fecha_inicial).getUTCMonth();
+        let dayS = new Date(horario.fecha_inicial).getUTCDate();
+        // date-fns
+        let fechaInicial = format(new Date(yearS, monthS, dayS), 'yyyy-MM-dd');
 
-          return false;
+        if ( horario.cant_matriculas > 0 && fechaInicial >= today) {
+          if (edad >= horario.edad_min && edad <= horario.edad_max) {
+            
+            return true;
+          } 
         }
+        
+        this.valores.valueHour = '';
+
+        return false;
       }
     )
 
@@ -135,7 +163,6 @@ export class FormComponent implements OnInit {
   // TODO: Crear matricula
   crearMatricula() {
     this.formSubmitted = true;
-    
     this.generarCodigo(10);
 
     // Verificar que el formulario es correcto al crear
@@ -164,6 +191,56 @@ export class FormComponent implements OnInit {
     if (this.enrollmentForm.get(campo)?.invalid && this.formSubmitted) return true;
     
     return false;
+  }
+
+  // TODO: Verificando el DNI para el template
+  verificarDNI() {
+    const dni = this.enrollmentForm.get('dni')?.value;
+
+    // Si la cantidad de números  no es 7
+    if (String(dni).length !== 7 && this.formSubmitted) return true;
+
+    return false;
+  }
+
+  // TODO: Validando el DNI para el form
+  validarDNI (dniValor: string): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const dniControl = formGroup.get(dniValor);
+
+      if (String(dniControl?.value).length === 7) {
+        dniControl?.setErrors(null);
+      } else {
+        dniControl?.setErrors({ noEsDNI: true})
+      }
+
+      return null;
+    }
+  }
+
+  // TODO: Verificando el n° celular para el template
+  verificarCelular() {
+    const celular = this.enrollmentForm.get('celular')?.value;
+
+    // Si la cantidad de números  no es 7
+    if (String(celular).length !== 9 && this.formSubmitted) return true;
+
+    return false;
+  }
+
+  // TODO: Validando el DNI para el form
+  validarCelular (celularValor: string): ValidatorFn {
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      const celularControl = formGroup.get(celularValor);
+
+      if (String(celularControl?.value).length === 9) {
+        celularControl?.setErrors(null);
+      } else {
+        celularControl?.setErrors({ noEsCelular: true})
+      }
+
+      return null;
+    }
   }
 
   // TODO: Entregará los valores que se seleccione en el select
