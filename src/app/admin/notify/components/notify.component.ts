1494feec692bad0ddc07f6814fe581b0b6fe275e
Jean Haro <JeanCHL98@hotmail.com>
2022-12-15 17:55:18 -0500

    Añadiendo las notificaciones en su modulo

--- src/app/admin/notify/components/notify.component.ts
+++ src/app/admin/notify/components/notify.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+// Observables
+import { Observable } from 'rxjs';
+
 // Font Awesome
 import { 
   faExclamation
 } from '@fortawesome/free-solid-svg-icons';
+
+// Interfaces
+import { NotifyForm } from 'src/app/interfaces/notify.interface';
+
+// Servicios
+import { NotifyService } from 'src/app/services/notify.service';
 
 @Component({
   selector: 'app-notify',
@@ -12,10 +21,17 @@ import {
 })
 export class NotifyComponent implements OnInit {
 
+  // Variables
+
   // Iconos
   faExclamation = faExclamation;
 
-  constructor() { }
+  // Variables - observable
+  notificaciones$!: Observable<NotifyForm[]>;
+
+  constructor (private notifyService: NotifyService) {
+    this.notificaciones$ = this.notifyService.notify$;
+  }
 
   ngOnInit(): void {
   }
--- src/app/admin/notify/notify.module.ts
+++ src/app/admin/notify/notify.module.ts
@@ -6,6 +6,7 @@ import { NotifyComponent } from './components/notify.component';
 
 // Modulos
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 // Routing
 import { NotifyRoutingModule } from './notify.routing';
@@ -17,7 +18,8 @@ import { NotifyRoutingModule } from './notify.routing';
   imports: [
     CommonModule,
     NotifyRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    SharedModule
   ]
 })
 export class NotifyModule { }
